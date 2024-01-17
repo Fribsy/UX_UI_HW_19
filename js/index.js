d04af6bd1d79fbf3ef19cdafdcf45c8cec9fce2d
Fribsy <prof.storm@gmail.com>
2024-01-17 12:38:53 -0800

    Added Hero Image Toggling on Click

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log('JS loaded');
 
-    // Hover
+    // Button Hover
     $('button').hover(
       function() {
         // Hover over
@@ -20,6 +20,7 @@ $(document).ready(function() {
     );
   });
 
+  // Img Hover
   $('img').hover(
     function() {
       // Hover over
@@ -34,3 +35,40 @@ $(document).ready(function() {
       });
     }
   );
+
+  // Navigation Text Bolding
+  $('.navigation li').hover(
+    function() {
+      // Hover over
+      $(this).css({
+        'font-weight': 'bold'
+      });
+    },
+    function() {
+      // Hover out
+      $(this).css({
+        'font-weight': ''
+      });
+    }
+  );
+
+// Hero Image Toggle 
+var isTransformed = false;
+$('.heroImg img').click(function() {
+  isTransformed = !isTransformed;
+
+  var imagePath = isTransformed ? 'images/Mountain.jpg' : 'images/Headshot.jpg';
+  $(this).attr('src', imagePath);
+
+  var originalWidth = $(this).data('original-width');
+  var originalHeight = $(this).data('original-height');
+  $(this).css({
+    'width': originalWidth,
+    'height': originalHeight
+  });
+});
+
+$('.heroImg img').each(function() {
+  $(this).data('original-width', $(this).width());
+  $(this).data('original-height', $(this).height());
+});
