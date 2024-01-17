fc407e2ea112c29d3ad67d64700daa24e1ac8d3b
Fribsy <prof.storm@gmail.com>
2024-01-17 15:33:58 -0800

    Final Code Tidying

--- js/index.js
+++ js/index.js
@@ -1,63 +1,63 @@
-$(document).ready(function() {
-    console.log('JS loaded');
+$(document).ready(function () {
+  console.log("JS loaded");
 
-    // Button Hover
-    $('button').hover(
-      function() {
-        $(this).css({
-          'background-color': '#8aabd1',
-          'color': '#333847'
-        });
-      },
-      function() {
-        $(this).css({
-          'background-color': '',
-          'color': ''
-        });
-      }
-    );
-  });
-
-  // Img Hover
-  $('img').hover(
-    function() {
+  // Button Hover
+  $("button").hover(
+    function () {
       $(this).css({
-        'transform': 'scale(1.1)'
+        "background-color": "#8aabd1",
+        color: "#333847",
       });
     },
-    function() {
+    function () {
       $(this).css({
-        'transform': ''
+        "background-color": "",
+        color: "",
       });
     }
   );
+});
 
-  $('.navigation li').hover(
-    function() {
-      $(this).addClass('bold');
-    },
-    function() {
-      $(this).removeClass('bold');
-    }
-  );
+// Img Hover
+$("img").hover(
+  function () {
+    $(this).css({
+      transform: "scale(1.1)",
+    });
+  },
+  function () {
+    $(this).css({
+      transform: "",
+    });
+  }
+);
 
-// Hero Image Toggle 
+$(".navigation li").hover(
+  function () {
+    $(this).addClass("bold");
+  },
+  function () {
+    $(this).removeClass("bold");
+  }
+);
+
+// Hero Image Toggle
 var isTransformed = false;
-$('.heroImg img').click(function() {
+$(".heroImg img").click(function () {
   isTransformed = !isTransformed;
 
-  var imagePath = isTransformed ? 'images/Mountain.jpg' : 'images/Headshot.jpg';
-  $(this).attr('src', imagePath);
+  var imagePath = isTransformed ? "images/Mountain.jpg" : "images/Headshot.jpg";
+  $(this).attr("src", imagePath);
 
-  var originalWidth = $(this).data('original-width');
-  var originalHeight = $(this).data('original-height');
+  var originalWidth = $(this).data("original-width");
+  var originalHeight = $(this).data("original-height");
   $(this).css({
-    'width': originalWidth,
-    'height': originalHeight
+    width: originalWidth,
+    height: originalHeight,
   });
 });
 
-$('.heroImg img').each(function() {
-  $(this).data('original-width', $(this).width());
-  $(this).data('original-height', $(this).height());
+$(".heroImg img").each(function () {
+  $(this).data("original-width", $(this).width());
+  $(this).data("original-height", $(this).height());
 });
